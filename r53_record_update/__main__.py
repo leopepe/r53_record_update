5d39d783ab213a92a603061e155b9cbbd23892f0
Leonardo Pepe de Freitas <leopepe@me.com>
2017-08-15 13:17:02 -0300

    datetime.now in str format and isoformat

--- r53_record_update/__main__.py
+++ r53_record_update/__main__.py
@@ -71,7 +71,7 @@ class R53:
         return self.list_private_zones_by_name(name)['Id'].split('/')[2]
 
     def update_a_record(self, record_name: str, ip: str, zone_id: str=None, ttl=600,
-                        comment: str='Updated at: {}'.format(str(datetime.now()))):
+                        comment: str='Updated at: {}'.format(str(datetime.now().isoformat(timespec='minutes')))):
         """
         Modifies a given resource record set for
         HostedZoneId
@@ -82,7 +82,7 @@ class R53:
         response = self.client.change_resource_record_sets(
             HostedZoneId=zone_id,
             ChangeBatch={
-                'Comment': comment,
+                'Comment': '{}'.format(comment),
                 'Changes': [
                     {
                         'Action': 'UPSERT',
